e927d4409bac6bc4c941d855959ddf72c620604a
ungureanudaniel <ungureanu.daniel86@gmail.com>
2020-06-30 14:13:01 +0300

    Security update

--- myresumeproject1/settings.py
+++ myresumeproject1/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'myresumeproject1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': config('DB_NAME'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
