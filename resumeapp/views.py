c1af0ee848a78daac7671c725f2e781c6ad6f667
danielungureanu <ungureanu.daniel86@gmail.com>
2020-08-07 00:40:51 +0300

    Update on profile

--- resumeapp/views.py
+++ resumeapp/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-from .models import Profile
 
 
 
 # the main view
 def home(request):
-    profile = Profile.objects.all().order_by('-id')[:1]
-
-    context = {
-        'profile': profile,
-    }
-    return render(request, 'resume1/home.html', context)
+    return render(request, 'resume1/home.html', {})
