201d2ab5e38da413c86b36a4111bdf1dc0656937
danielungureanu <ungureanu.daniel86@gmail.com>
2020-08-08 14:11:39 +0300

    Update

--- myresumeproject1/settings.py
+++ myresumeproject1/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '6rgj4c1nb)fdu3f(zj+l5+2k=ono$txwhid=th645mb^s#gvxy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.heroku.com']
 
