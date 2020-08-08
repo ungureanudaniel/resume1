83d9a4880861d1957d47726cfd3a02dfcbf4e354
danielungureanu <ungureanu.daniel86@gmail.com>
2020-08-08 23:48:19 +0300

    DEbug update

--- myresumeproject1/settings.py
+++ myresumeproject1/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['fierce-turmeric-wosexrdlqhzyk396c1a772kn.herokudns.com']
 
