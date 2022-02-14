d816f09a85fa52aae47f87ca8b6c91684ef434bb
ungureanudaniel <ungureanu.daniel86@gmail.com>
2022-02-14 15:55:10 +0200

    Added portfolio items, changed superuser

--- myresumeproject1/settings.py
+++ myresumeproject1/settings.py
@@ -140,10 +140,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-
-
-
-
 #------------------AWS SETTINGS ---------------------------------------
 
 
