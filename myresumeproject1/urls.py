b7b9d1d8a01accbe1c85f2f5f411fc684d58b485
danielungureanu <ungureanu.daniel86@gmail.com>
2023-10-09 23:38:36 +0300

    Repaired owl.video-play error in vendor folder

--- myresumeproject1/urls.py
+++ myresumeproject1/urls.py
@@ -1,7 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.views.static import serve
 from django.contrib.sitemaps.views import sitemap
 from django.conf.urls.static import static
 from resumeapp.sitemaps import StaticViewSitemap
@@ -9,7 +10,6 @@ from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
 from resumeapp.sitemaps import *
-# from django.conf.urls import handler404, handler500
 sitemaps = {
     'static': StaticViewSitemap,
     'skillsitemap':SkillSitemap,
@@ -19,10 +19,14 @@ sitemaps = {
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('resumeapp.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
-    path("robots.txt", TemplateView.as_view(template_name="services/robots.txt", content_type="text/plain"),),
+    path("robots.txt", TemplateView.as_view(template_name="resume1/robots.txt", content_type="text/plain"),),
 ]
+urlpatterns += i18n_patterns(
+    path('', include('resumeapp.urls')),
+
+)
+urlpatterns += re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
