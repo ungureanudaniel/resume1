5c6bdb6678be9822ca5d75f03450e59398c63efe
danielungureanu <ungureanu.daniel86@gmail.com>
2023-10-10 11:18:17 +0300

    Restored pdf cv version

--- resumeapp/forms.py
+++ resumeapp/forms.py
@@ -1,5 +1,6 @@
 from .models import Profile
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 class ProfileForm(forms.ModelForm):
 
@@ -8,12 +9,12 @@ class ProfileForm(forms.ModelForm):
         fields = ['name', 'text', 'age', 'study', 'current_job', 'city']
 
         widgets = {
-            'name': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add name...'}),
-            'text': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add your profile...'}),
-            'age': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add your age...'}),
-            'study': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add your studies level and university...'}),
-            'current_job': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add your current job...'}),
-            'city': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add your current city...'}),
+            'name': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': _('Add name...')}),
+            'text': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': _('Add your profile...')}),
+            'age': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': _('Add your age...')}),
+            'study': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': _('Add your studies level and university...')}),
+            'current_job': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': _('Add your current job...')}),
+            'city': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': _('Add your current city...')}),
         }
 
 
@@ -23,8 +24,8 @@ class ContactForm(forms.Form):
         fields = ['name', 'email', 'message']
 
         widgets = {
-            'name': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add name...'}),
-            'email': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add your email...'}),
-            'message': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': 'Add a few words...'}),
+            'name': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': _('Add name...')}),
+            'email': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': _('Add your email...')}),
+            'message': forms.TextInput(attrs = {'class': 'form-control', 'placeholder': _('Add a few words...')}),
 
         }
